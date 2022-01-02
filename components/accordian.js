5ce88bd2187a7a2af471e192925d05c3be69297b
EliavYair1 <77952937+EliavYair1@users.noreply.github.com>
2022-01-02 09:00:19 +0200

    Update accordian.js

--- components/accordian.js
+++ components/accordian.js
@@ -1,17 +1,26 @@
 import { motion } from "framer-motion"
 
 export default function Accordian({ active, onClick }) {
+  const Hvariants = {
+    initial: {
+      height: false,
+      opacity: 0
+    },
+    animate: {
+      height: active ? "auto" : "0px",
+      opacity: 1
+    }
+  };
 	return (
 		<div className="relative block" onClick={onClick}>
 			<div className="my-5">Accordian</div>
 
 			<motion.div
-				initial={false}
+				 variants={Hvariants}
+        initial="initial"
+animate="animate"
 				className="relative overflow-hidden"
-				animate={{
-					height: active ? "auto" : "0px",
-					opacity: active ? 1 : 0
-				}}
+				
 				transition={{ duration: 0.5, ease: [0.7, 0, 0.3, 1] }}
 			>
 				<div className="mb-4 h-[70vh] w-full bg-black"></div>
